82900bf900dc20b7d593f12d6a1243b06fdad04f
Sami Younas <samiys005@gmail.com>
2020-07-21 16:19:09 +0500

    New Design & Features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import Message from "./Message";
+import { Button } from 'react-bootstrap-buttons';
+import 'react-bootstrap-buttons/dist/react-bootstrap-buttons.css';
+
 
 function App() {
 
@@ -9,22 +12,34 @@ function App() {
 
   return (
       <div className={`box ${isMorning ? 'dayLight' : 'box'}`}>
+          <div className="limiter">
+              <div className="container-login100">
+                  <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+                      <h1 style={{}} className="login100-form-title">
+                          UPDATE COUNTER & DAY
+					</h1>
+                      <br/>
+
 
         <h1>GOOD {isMorning ? 'MORNING' : 'NIGHT'}</h1>
 
         <Message counter = {count}/>
+        <br/>
 
-        <button onClick={
+        <Button sm btnStyle="primary" variant="primary" className="btn-primary" onClick={
           () => setCount(++count)
         }>
           Update Counter
-        </button>
+        </Button>{' '}
 
-        <button onClick={
+        <Button sm btnStyle="warning" variant="warning" onClick={
           () => setMorning(!isMorning)
         }>
           Update Day
-        </button>
+        </Button>
+              </div>
+          </div>
+      </div>
       </div>
   );
 }

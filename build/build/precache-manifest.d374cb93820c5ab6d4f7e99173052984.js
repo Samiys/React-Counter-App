742b698be5dbf9c4912a8bfe1f548fe349beb09d
Sami Younas <samiys005@gmail.com>
2020-07-21 18:55:44 +0500

    Add files via upload

---
+++ build/build/precache-manifest.d374cb93820c5ab6d4f7e99173052984.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e281ee829fc592f35148268cc380ebf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b590688b117dc615b2c1",
+    "url": "/static/css/2.a891329e.chunk.css"
+  },
+  {
+    "revision": "59910dd93d00d911d6b2",
+    "url": "/static/css/main.dd3c337e.chunk.css"
+  },
+  {
+    "revision": "b590688b117dc615b2c1",
+    "url": "/static/js/2.60f44981.chunk.js"
+  },
+  {
+    "revision": "cde06cf9ac38a1a04d25807bf2202a79",
+    "url": "/static/js/2.60f44981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59910dd93d00d911d6b2",
+    "url": "/static/js/main.26141949.chunk.js"
+  },
+  {
+    "revision": "e744f14f3a88f64dcdb4",
+    "url": "/static/js/runtime-main.ea513989.js"
+  },
+  {
+    "revision": "bcfaa15f2479f2213946f8eeb5ccbe95",
+    "url": "/static/media/morning.bcfaa15f.jpg"
+  },
+  {
+    "revision": "c0c9b4aa7542b09d27377d7547019019",
+    "url": "/static/media/night.c0c9b4aa.jpg"
+  }
+]);
